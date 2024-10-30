f3a347ef2858a73dd697f99ddfc2bd1e2fa9fad5
maria <maria@MARIAJOSE2306>
2024-10-30 08:03:39 -0700

    Agregar archivos de proyecto.

---
+++ MetodosFuncionesNormal/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace MetodosFuncionesNormal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MostrarMensaje("Mensaje Desde el Metodo...1");
+            MostrarMensaje("Mesaje Desde el Metodo...2");
+
+            Sumar(2, 5);
+            Sumar(3, 9);
+        }
+        //el Program siempre va a llevar su Main
+        static void MostrarMensaje(string mensaje)
+        {
+            Console.WriteLine(mensaje);
+        }
+        static void Sumar(int primerNumero, int segundoNumero)
+        {
+            int total = primerNumero + segundoNumero;
+            MostrarMensaje($"el resultado de la suma de {primerNumero} y {segundoNumero} es {total}");
+        }
+    }
+}
