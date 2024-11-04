79f6dd771a623a95ae02d95251ff1fb463ac79dd
Majo-2306 <maria_castro24@unikino.edu.mx>
2024-11-04 08:38:08 -0700

    #nueva edicion

--- MetodosFuncionesNormal/Program.cs
+++ MetodosFuncionesNormal/Program.cs
@@ -4,21 +4,28 @@
     {
         static void Main(string[] args)
         {
+            string miMensaje = "Hola";
             MostrarMensaje("Mensaje Desde el Metodo...1");
             MostrarMensaje("Mesaje Desde el Metodo...2");
+            MostrarMensaje(miMensaje);
 
-            Sumar(2, 5);
+            int primerSuma = Sumar(2, 5);
+            MostrarMensaje($"El resultado de la primera suma es {primerSuma} ");
+           // MostrarMensaje($"el resultado de la suma de {primerNumero} y {segundoNumero} es {total}");
             Sumar(3, 9);
+
         }
         //el Program siempre va a llevar su Main
         static void MostrarMensaje(string mensaje)
         {
             Console.WriteLine(mensaje);
         }
-        static void Sumar(int primerNumero, int segundoNumero)
+
+        //Función
+        static int Sumar(int primerNumero, int segundoNumero)
         {
             int total = primerNumero + segundoNumero;
-            MostrarMensaje($"el resultado de la suma de {primerNumero} y {segundoNumero} es {total}");
+            return total;
         }
     }
 }
